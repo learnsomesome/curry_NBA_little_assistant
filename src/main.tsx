4403e34566a8c669ea2153cc2987ddefc9137b43
curry <767329910@qq.com>
2022-12-08 15:58:17 +0800

    add basename

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import { MemoryRouter } from 'react-router-dom'
+import { BASE_PATH } from './utils/constants'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <MemoryRouter initialEntries={['/players']}>
+    <MemoryRouter initialEntries={['/players']} basename={BASE_PATH}>
       <App />
     </MemoryRouter>
 )
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
 export const BASE_URL = 'https://free-nba.p.rapidapi.com';
+export const BASE_PATH = '/curry_NBA_little_assistant';
 export const RAPID_API_KEY = '519013befbmsh499a8abc9bdfb92p1dbb76jsn3dcbab152f7a';
 export const RAPID_API_HOST = 'free-nba.p.rapidapi.com';
